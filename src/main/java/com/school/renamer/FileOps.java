db74b5dac2dc40fe63c25d67ce63f38d39a1fa6c
aleksiolivier <60711750+aleksiolivier@users.noreply.github.com>
2021-11-17 15:53:18 -0400

    reset

--- src/main/java/com/school/renamer/FileOps.java
+++
@@ -1,7 +0,0 @@
-package com.school.renamer;
-import java.nio.file.CopyOption;
-import java.nio.file.Path;
-
-public interface FileOps {
-    public void copyFile(Path srcFile, Path destFile, CopyOption option);
-}
